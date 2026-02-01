3565c1e0ef895ba3c7dc7325b3f27fb67efd9e3c
Nathan-Amiri <convenientlyoblivious@gmail.com>
2026-02-01 11:56:46 -0600

    Adjusted teleporter

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Teleporter : MonoBehaviour
 {
     [SerializeField] private Vector2 direction;
+
+    public GameObject circleIndicator;
+    private void Awake()
+    {
+        circleIndicator.SetActive(false);
+    }
     public void OnTriggerEnter2D(Collider2D col)
     {
         if (col.CompareTag("Player"))
